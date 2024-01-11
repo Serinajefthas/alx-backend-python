2aebe38d6bf58eec64d4a9d661d1ccc4dc8c5964
Serinajefthas <serina.jefthas@gmail.com>
2024-01-11 19:36:07 +0000

    add readmes and tasks 0-2

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """Method calculates sum float args and returns float value"""
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import math
+
+
+def floor(n: float) -> floor:
+    """Method that returns floor(largest int <= num) of float number"""
+    return math.floor(n)
