20e7b0edaf6a506115512088ae1e2b1c58245d7c
Serinajefthas <serina.jefthas@gmail.com>
2024-01-12 10:38:57 +0000

    fix task 7 and 9

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """a typed annotated function"""
 from typing import Union, List, Tuple
-from numbers import Real
 
 
-def to_kv(k: str, v: Real) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Method takes str and int/float and returns tuple"""
-    return k, v * v
+    return (k, float(v*v))
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """a typed annotated function"""
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """function is passed list and returns tuple with the string
     and the length of that string"""
     return [(i, len(i)) for i in lst]
