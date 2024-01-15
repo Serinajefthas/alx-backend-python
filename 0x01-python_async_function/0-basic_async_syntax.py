36eef16aa28bbb2794aa83ad6cb57459a1d1f9b7
Serinajefthas <serina.jefthas@gmail.com>
2024-01-15 22:25:13 +0000

    add type annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,10 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Function waits for random delay then returns"""
     random_delay = random.uniform(0.0, max_delay)
     await asyncio.sleep(random_delay)
+    return random_delay
+
+asyncio.run(wait_random())
