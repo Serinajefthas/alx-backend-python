95b2485620166e405362e7f6f48a20e983cc99f5
Serinajefthas <serina.jefthas@gmail.com>
2024-01-16 22:01:07 +0000

    correct for loop task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,8 +8,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measures total runtime of async comp when run 4x in parallel"""
     start_time = asyncio.get_event_loop().time()
-    for i in range(4):
-        await asyncio.gather(async_comprehension())
+    """cant use for loop as just wait for executing next coroutine, but
+    want call it asynchronously in this case"""
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     end_time = asyncio.get_event_loop().time()
     runtime = end_time - start_time
     return runtime
