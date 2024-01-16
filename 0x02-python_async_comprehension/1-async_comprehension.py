a3d4d10c052a85d0e73666cd6ad14cf65372cfad
Serinajefthas <serina.jefthas@gmail.com>
2024-01-16 21:34:48 +0000

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module to create async comprehension"""
+from typing import List
+
+async_generator = importlib.import_module('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """collects random nums from async generator
+    into list then returns the list"""
+    results = [await i async for i in async_generator()]
+    return results
